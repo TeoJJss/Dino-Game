91f58e053e37b01c8e6dee56c0c76cd452dfd1c1
TeoJJss <teojunjia@gmail.com>
2022-10-11 21:27:34 +0800

    init again

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,19 @@
 from urllib import request
 from flask import render_template
 from app import app
-import os
+import os, pandas as pd
 
 global ls
 ls=[]
 
 @app.route('/<string:content>', methods=['POST'])
-def index(content):
+def index(content, ls):
     return ls.append(content)
 
 @app.route('/')
 def display():
-    return render_template("index.html", lis=ls)
+    final=[*ls, *ls]
+    return render_template("index.html", lis=final)
 
 if __name__ == "__main__":
     app.run()
