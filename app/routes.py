a9eb0b470db85d8ba4a96bbb970a9c1f7c73b1c4
TeoJJss <teojunjia@gmail.com>
2022-10-12 15:33:44 +0800

    sorted

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 # from crypt import methods
+import string
 from urllib import request
 from flask import render_template, Response
 from app import app
-import os, json
+import os, json, re
 
 # global ls
 # ls=[]
@@ -16,13 +17,34 @@ def index(content):
 @app.route('/')
 def display():
     ls=[]
-    try:
-        with open ("lb.txt", "r") as f:
-            content = f.readlines()
-            return render_template("index.html", lis=content)
-    except:
-        pass
+    file="lb.txt"
+
+    lines = open(file).read().splitlines()
+    splitted = (line.split(',') for line in lines)
+    items = ((i[0], int(i[1])) for i in splitted)
+
+    # Reversed sort
+    reversed_items = sorted(items, key=lambda i: -i[1])
+
+    open(file, 'w+').write('\n'.join((f"{i[0]}, {i[1]}" for i in reversed_items)))
+    # with open (file, "r") as f:
+    #     for line in f:
+    #         temp = line.split()
+    #         for i in temp:
+    #             ls.append(i)
+    # with open(file, "w+") as f:
+    #     for i in ls:
+    #         f.writelines(i)
+    #         f.writelines("\n")
     
+    content=open(file).readlines()
+    return render_template("index.html", lis=content)
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 if __name__ == "__main__":
     app.run()
